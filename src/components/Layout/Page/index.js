135b177d71fc528ed36073a67310d3837afa27f6
NKBelousov <belousovnikita92@gmail.com>
2018-02-18 14:06:40 +0300

    add page component

---
+++ src/components/Layout/Page/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Content = styled.div`
+  border-radius: 5px;
+  box-shadow: 0 0 5px #000;
+  margin: 1rem 0 0 0;
+  padding: 1rem;
+`;
+
+class Page extends Component {
+  static get propTypes() {
+    return {
+      children: PropTypes.node,
+    };
+  }
+  render() {
+    return <Content>{this.props.children}</Content>;
+  }
+}
+
+export default Page;
