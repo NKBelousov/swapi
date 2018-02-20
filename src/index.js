53800a6458a15aabc4580345a96cd90d0f9cd3e1
NKBelousov <belousovnikita92@gmail.com>
2018-02-21 00:55:29 +0300

    add redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import { render } from "react-dom";
 
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
 import app from "~/reducers";
 import App from "~/components/App";
 
-const store = createStore(app);
+const store = createStore(app, devToolsEnhancer());
 
 render(
   <Provider store={store}>
