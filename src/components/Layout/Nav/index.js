1343620a996faab52ac848d74066f3c1982498ad
NKBelousov <belousovnikita92@gmail.com>
2018-02-25 23:39:35 +0300

    remove unused css module from nav

--- src/components/Layout/Nav/index.js
+++ src/components/Layout/Nav/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { map } from "lodash";
 import PropTypes from "prop-types";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 const Navigation = styled.nav`
   background: red;
