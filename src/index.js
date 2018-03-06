412260ba28aad7a39d7bef1e09c36db7052c02c0
NKBelousov <belousovnikita92@gmail.com>
2018-03-06 21:19:40 +0300

    add redux devtools

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ import { createStore, applyMiddleware } from "redux";
 import reducers from "~/reducers";
 import App from "~/components/App";
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+);
 
 render(
   <Provider store={store}>
