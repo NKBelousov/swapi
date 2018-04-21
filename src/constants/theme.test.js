c7e70ff68dbd3808e0b237b895acee7a694a5a31
NKBelousov <belousovnikita92@gmail.com>
2018-04-21 06:25:49 +0300

    add test for theme

---
+++ src/constants/theme.test.js
@@ -0,0 +1,15 @@
+import * as THEME from "./theme.js";
+import { each } from "lodash";
+
+test("Every value is unique", () => {
+  const theme = [];
+  let result = true;
+  each(THEME, m => {
+    if (theme.indexOf(m) === -1) {
+      theme.push(m);
+    } else {
+      result = false;
+    }
+    expect(result).toBe(true);
+  });
+});
