f847a6c69b82cd86d8c86d48d780cc829d0f8f59
NKBelousov <belousovnikita92@gmail.com>
2019-02-06 20:54:23 +0300

    inject package version into template

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackAutoInjectVersion = require("webpack-auto-inject-version");
 
 module.exports = {
   resolve: {
@@ -23,5 +24,24 @@ module.exports = {
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      environment: process.env.NODE_ENV,
+      filename: __dirname + "/dist/index.html",
+      hash: true,
+      template: __dirname + "/src/template.ejs",
+      title: "Swapi Client",
+    }),
+    new WebpackAutoInjectVersion({
+      components: {
+        InjectAsComment: true,
+      },
+      componentsOptions: {
+        InjectAsComment: {
+          tag: `Package version: {version} - {date}`,
+          dateFormat: "yyyy-mm-dd HH:MM",
+        },
+      },
+    }),
+  ],
 };
