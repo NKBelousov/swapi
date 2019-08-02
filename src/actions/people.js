958903e5c7758e0082206f9a623d81539429b8b2
NKBelousov <belousovnikita92@gmail.com>
2019-08-02 23:52:52 +0300

    better values for people actions

--- src/actions/people.js
+++ src/actions/people.js
@@ -1,25 +1,25 @@
-export const REQUEST_PEOPLE = "REQUEST_PEOPLE";
+export const REQUEST_PEOPLE = "@@people/REQUEST";
 export const requestPeople = () => ({
   type: REQUEST_PEOPLE,
 });
 
-export const FETCH_PEOPLE = "FETCH_PEOPLE";
+export const FETCH_PEOPLE = "@@people/FETCH";
 export const fetchPeople = () => ({
   type: FETCH_PEOPLE,
 });
 
-export const FETCH_PEOPLE_FAILURE = "FETCH_PEOPLE_FAILURE";
+export const FETCH_PEOPLE_FAILURE = "@@people/FETCH_FAILURE";
 export const fetchPeopleFailure = () => ({
   type: FETCH_PEOPLE_FAILURE,
 });
 
-export const FETCH_PEOPLE_SUCCESS = "FETCH_PEOPLE_SUCCESS";
+export const FETCH_PEOPLE_SUCCESS = "@@people/FETCH_SUCCESS";
 export const fetchPeopleSuccess = data => ({
   type: FETCH_PEOPLE_SUCCESS,
   data,
 });
 
-export const GO_TO_PEOPLE = "GO_TO_PEOPLE";
+export const GO_TO_PEOPLE = "@@people/GO_TO";
 export const goToPeople = () => ({
   type: GO_TO_PEOPLE,
   url: "/people",
