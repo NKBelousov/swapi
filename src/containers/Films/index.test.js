c08e94369df664a4d6cd1f445611fca823a06a21
NKBelousov <belousovnikita92@gmail.com>
2019-11-18 18:44:31 +0300

    fix films tests

--- src/containers/Films/index.test.js
+++ src/containers/Films/index.test.js
@@ -1,28 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { ThemeProvider } from "styled-components";
 
-import * as statuses from '~/constants/statuses'
+import theme from "~/constants/theme";
+import * as statuses from "~/constants/statuses";
+import rootReducer from "~/reducers";
 
-import Films from './index'
+import Films from "./index";
 
-describe('Films', () => {
-  let div
+describe("Films", () => {
+  let div;
   beforeEach(() => {
-    div = document.createElement('div')
-  })
+    div = document.createElement("div");
+  });
   afterEach(() => {
-    ReactDOM.unmountComponentAtNode(div)
-  })
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  const store = createStore(rootReducer);
 
   Object.values(statuses).map(status => {
-    it.skip(`should render status "${status}" without crashing`, () => {
-      ReactDOM.render(<Films
-        data={{
-          data: [],
-          status: status
-        }}
-        onRequest={jest.fn()}
-      />, div)
-    })
-  })
-})
+    it(`should render status "${status}" without crashing`, () => {
+      ReactDOM.render(
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>
+            <Router>
+              <Films
+                data={{
+                  data: [],
+                  status: status,
+                }}
+                onRequest={jest.fn()}
+              />
+            </Router>
+          </ThemeProvider>
+        </Provider>,
+        div
+      );
+    });
+  });
+});
