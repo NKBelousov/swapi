fea2e5e794b8e352e3a18fd1ffb4c881f0a147f4
NKBelousov <belousovnikita92@gmail.com>
2019-11-22 00:05:31 +0300

    fix film tests

--- src/components/Film/index.test.js
+++ src/components/Film/index.test.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 
-import Film from './index'
+import Film from "./index";
+import theme from "~/constants/theme";
 
-describe('Film', () => {
+describe("Film", () => {
   it(`should render without crashing`, () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<Film />, div)
-    ReactDOM.unmountComponentAtNode(div)
-  })
-})
+    const div = document.createElement("div");
+    ReactDOM.render(
+      <ThemeProvider theme={theme}>
+        <Film />
+      </ThemeProvider>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
