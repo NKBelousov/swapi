0a9aa17bb1eb07e79e48ae02ea18bba2259cb8fa
NKBelousov <belousovnikita92@gmail.com>
2019-11-23 08:51:41 +0300

    fix page tests

--- src/components/Layout/Page/index.test.js
+++ src/components/Layout/Page/index.test.js
@@ -1,15 +1,19 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { ThemeProvider } from "styled-components";
 
 import Page from "./index.js";
+import theme from "~/constants/theme";
 
 test("Should render correctly", () => {
   const tree = renderer
     .create(
-      <Page>
-        <h1>Header</h1>
-        <h2>Subheader</h2>
-      </Page>
+      <ThemeProvider theme={theme}>
+        <Page>
+          <h1>Header</h1>
+          <h2>Subheader</h2>
+        </Page>
+      </ThemeProvider>
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
