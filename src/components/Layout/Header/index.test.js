3883f3256c181aa4057839c235e62e9009ebc52d
NKBelousov <belousovnikita92@gmail.com>
2019-11-28 06:31:26 +0300

    fix header tests

--- src/components/Layout/Header/index.test.js
+++ src/components/Layout/Header/index.test.js
@@ -1,14 +1,18 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { ThemeProvider } from "styled-components";
 
 import Header from "./index.js";
+import theme from "~/constants/theme";
 
 test("Should render correctly", () => {
   const tree = renderer
     .create(
-      <Header tooltip="Test">
-        <h1>Children</h1>
-      </Header>
+      <ThemeProvider theme={theme}>
+        <Header tooltip="Test">
+          <h1>Children</h1>
+        </Header>
+      </ThemeProvider>
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
