b729bbd477e15049b6865bc1a12be5460b917864
NKBelousov <belousovnikita92@gmail.com>
2019-11-29 07:43:39 +0300

    fix data fetcher tests

--- src/components/Utility/DataFetcher/index.test.js
+++ src/components/Utility/DataFetcher/index.test.js
@@ -1,20 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import * as Statuses from '~/constants/statuses'
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 
-import DataFetcher from './'
+import DataFetcher from "./";
+import * as Statuses from "~/constants/statuses";
+import theme from "~/constants/theme";
 
-describe('DataFetcher', () => {
-  test.each(Object.values(Statuses))(`should render %p without crashing`, status => {
-    const div = document.createElement('div')
-    ReactDOM.render(<DataFetcher data={{
-      status,
-      data: ['Test'],
-    }}
-      onRequest={jest.fn()}
-    >
-      {data => data}
-    </DataFetcher>, div)
-    ReactDOM.unmountComponentAtNode(div)
-  })
-})
+describe("DataFetcher", () => {
+  test.each(Object.values(Statuses))(
+    `should render %p without crashing`,
+    status => {
+      const div = document.createElement("div");
+      ReactDOM.render(
+        <ThemeProvider theme={theme}>
+          <DataFetcher
+            data={{
+              status,
+              data: ["Test"],
+            }}
+            onRequest={jest.fn()}>
+            {data => data}
+          </DataFetcher>
+        </ThemeProvider>,
+        div
+      );
+      ReactDOM.unmountComponentAtNode(div);
+    }
+  );
+});
