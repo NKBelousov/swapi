36c2436ab1a2b452d8678c6a199741421f797651
NKBelousov <belousovnikita92@gmail.com>
2019-11-30 08:01:04 +0300

    fix page not found tests

--- src/components/PageNotFound/index.test.js
+++ src/components/PageNotFound/index.test.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 
-import PageNotFound from './index'
+import PageNotFound from "./index";
+import theme from "~/constants/theme";
 
-describe('PageNotFound', () => {
+describe("PageNotFound", () => {
   it(`should render without crashing`, () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<PageNotFound />, div)
-    ReactDOM.unmountComponentAtNode(div)
-  })
-})
+    const div = document.createElement("div");
+    ReactDOM.render(
+      <ThemeProvider theme={theme}>
+        <PageNotFound />
+      </ThemeProvider>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
