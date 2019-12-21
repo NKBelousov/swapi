498d2e87e995eb9e16a2b436fdf92b2efaffc798
NKBelousov <belousovnikita92@gmail.com>
2019-12-21 07:11:23 +0300

    add more data to person info component

--- src/containers/PersonInfo/index.js
+++ src/containers/PersonInfo/index.js
@@ -1,17 +1,59 @@
-import React, { memo } from "react";
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
 import { useSelector } from "react-redux";
+import React, { memo } from "react";
+import styled from "styled-components";
+
 import { getPersonByName } from "~/selectors/getPersonByName";
 
 const Content = styled.div`
   color: ${props => props.theme.secondary};
+  text-align: center;
+`;
+
+const Name = styled.h1`
+  font-weight: bold;
+`;
+
+const Item = styled.h2`
+  text-align: center;
+  opacity: 0.8;
+`;
+
+const ItemValue = styled.span`
+  letter-spacing: 0.25ch;
+  margin-left: 0.5em;
+  text-decoration-color: ${props => props.theme.secondary};
+  text-decoration-style: dotted;
+  text-decoration: underline;
+  text-transform: uppercase;
 `;
 
 const PersonInfo = memo(() => {
   const { name } = useParams();
   const person = useSelector(getPersonByName(name));
-  return <Content>{JSON.stringify(person, null, 2)}</Content>;
+
+  return (
+    <Content>
+      <Name>
+        {person.name} {person.gender === "male" ? "♂" : "♀"}
+      </Name>
+      <Item>
+        Height: <ItemValue>{person.height}</ItemValue>
+      </Item>
+      <Item>
+        Mass: <ItemValue>{person.mass}</ItemValue>
+      </Item>
+      <Item>
+        Hair color: <ItemValue>{person.hair_color}</ItemValue>
+      </Item>
+      <Item>
+        Skin color: <ItemValue>{person.skin_color}</ItemValue>
+      </Item>
+      <Item>
+        Eye color: <ItemValue>{person.eye_color}</ItemValue>
+      </Item>
+    </Content>
+  );
 });
 
 PersonInfo.displayName = "PersonInfo";
